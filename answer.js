bb6ae50dc3b56693ddbf70640061ee233621611d
Amanda Nunn <amandanunn@thearrayschool.com>
2018-12-07 08:54:20 -0700

    final solution for challenge 6

--- answer.js
+++ answer.js
@@ -1,1 +1,8 @@
-// *** YOUR ANSWER BELOW ***
+const GAL_TO_PINTS = 8;
+const PINTS_TO_CUPS = 2;
+
+function gallonsToCups(gallons){
+  let pints = gallons * GAL_TO_PINTS;
+  let cups = pints * PINTS_TO_CUPS;
+  return cups;
+}
